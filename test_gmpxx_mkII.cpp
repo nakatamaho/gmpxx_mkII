3811b7b706890347001cc7b9f1d6d4b8e8f2854c
NAKATA Maho <maho.nakata@gmail.com>
2024-03-05 11:14:58 +0900

    .

--- test_gmpxx_mkII.cpp
+++ test_gmpxx_mkII.cpp
@@ -36,26 +36,20 @@
 using namespace gmp;
 
 // Asserts that the mpf_class object equals the expected string representation
-bool IsGMPEquals(mpf_class &gmpobj, const char *expected, int base = defaults::base, int precision = 10) {
+bool Is_mpf_class_Equals(mpf_class &gmpobj, const char *expected, int base = defaults::base, int precision = 10) {
     char formatString[64];
     char buffer[64];
 
     // Adjust the comparison based on the base
     switch (base) {
-    case 2:
-        std::sprintf(formatString, "%%.%dRb", precision);      // Generates format string like "%.10Rf"
-        gmp_sprintf(buffer, formatString, gmpobj.get_mpf_t()); // Uses generated format string
-        break;
     case 10:
-        std::sprintf(formatString, "%%.%dRf", precision);      // Generates format string like "%.10Rf"
+        std::sprintf(formatString, "%%.%dFf", precision);      // Generates format string like "%.10Ff"
         gmp_sprintf(buffer, formatString, gmpobj.get_mpf_t()); // Uses generated format string
         break;
-
     case 16:
-        std::sprintf(formatString, "%%.%dRa", precision);      // Generates format string like "%.10Rf"
+        std::sprintf(formatString, "%%.%dFa", precision);      // Generates format string like "%.10Fa"
         gmp_sprintf(buffer, formatString, gmpobj.get_mpf_t()); // Uses generated format string
         break;
-
     default:
         printf("not supported");
         exit(-1);
@@ -82,8 +76,9 @@ void testDefaultPrecision() {
 void testDefaultConstructor() {
     mpf_class a;
     char buffer[100];
-    gmp_snprintf(buffer, sizeof(buffer), "%.0Rf", a.get_mpf_t());
-    assert(std::string(buffer) == "nan");
+    gmp_snprintf(buffer, sizeof(buffer), "%.0Ff", a.get_mpf_t());
+    assert(std::string(buffer) == "0");
+    // initialized to zero
     std::cout << "Default constructor test passed." << std::endl;
 }
 
@@ -107,22 +102,130 @@ void testInitializationAndAssignmentDouble() {
     const char *expectedValue = "3.1415926535";
 
     gmp::mpf_class a = (mpf_class)testValue;
-    assert(IsGMPEquals(a, expectedValue));
+    assert(Is_mpf_class_Equals(a, expectedValue));
     std::cout << "Substitution from double using constructor test passed." << std::endl;
 
     gmp::mpf_class b;
     b = testValue;
-    assert(IsGMPEquals(b, expectedValue));
+    assert(Is_mpf_class_Equals(b, expectedValue));
     std::cout << "Substitution from double using assignment test passed." << std::endl;
 }
 
+void testInitializationAndAssignmentString() {
+    // Testing initialization with a decimal number using a constructor
+    const char *expectedDecimalValue = "1.4142135624";
+    mpf_class a = expectedDecimalValue;
+    assert(Is_mpf_class_Equals(a, expectedDecimalValue));
+    std::cout << "Constructor initialization with decimal '" << expectedDecimalValue << "' test passed." << std::endl;
+
+    // Testing initialization with a decimal number using an assignment operator
+    mpf_class b;
+    b = expectedDecimalValue;
+    assert(Is_mpf_class_Equals(b, expectedDecimalValue));
+    std::cout << "Assignment initialization with decimal '" << expectedDecimalValue << "' test passed." << std::endl;
+
+    // Testing initialization with a decimal number using a constructor
+    std::string expectedDecimalValueString = "3.1415926535";
+    mpf_class c = expectedDecimalValueString;
+    assert(Is_mpf_class_Equals(c, expectedDecimalValueString.c_str()));
+    std::cout << "Constructor initialization with decimal '" << expectedDecimalValueString << "' test passed." << std::endl;
+
+    // Testing initialization with a decimal number using an assignment operator
+    mpf_class d;
+    d = expectedDecimalValueString;
+    assert(Is_mpf_class_Equals(d, expectedDecimalValueString.c_str()));
+    std::cout << "Assignment initialization with decimal '" << expectedDecimalValueString << "' test passed." << std::endl;
+
+    // Testing initialization with a hexadecimal number using an assignment operator
+    const char *expectedHexValue = "0x3.243f6a8885a3p+0";
+    const char *inputHexValue = "3.243F6A8885A308D313198A2E03707344A4093822299F31D008";
+    mpf_class e(inputHexValue, 16);
+    assert(Is_mpf_class_Equals(e, expectedHexValue, 16, 12));
+    std::cout << "Assignment initialization with hexadecimal '" << expectedHexValue << "' test passed." << std::endl;
+
+    defaults::base = 16;
+    // Testing initialization with a hexadecimal number using a constructor
+    mpf_class f;
+    e = inputHexValue;
+    assert(Is_mpf_class_Equals(e, expectedHexValue, defaults::base, 12));
+    std::cout << "Constructor initialization with hexadecimal '" << expectedHexValue << "' test passed." << std::endl;
+    defaults::base = 10;
+}
+
+void testAddition() {
+    mpf_class a(1.5);
+    mpf_class b(2.5);
+    std::string expected = "4.0000000000";
+
+    mpf_class c = a + b;
+    assert(Is_mpf_class_Equals(c, expected.c_str()));
+    a += b;
+    assert(Is_mpf_class_Equals(a, expected.c_str()));
+    std::cout << "Addition Test passed." << std::endl;
+}
+
+void testMultplication() {
+    mpf_class a(2.0);
+    mpf_class b(3.0);
+    std::string expected = "6.0000000000";
+
+    mpf_class c = a * b;
+    assert(Is_mpf_class_Equals(c, expected.c_str()));
+    a *= b;
+    assert(Is_mpf_class_Equals(a, expected.c_str()));
+    std::cout << "Multiplication Test passed." << std::endl;
+}
+
+void testDivision() {
+    mpf_class a(6.0);
+    mpf_class b(2.0);
+    std::string expected = "3.0000000000";
+
+    mpf_class c = a / b;
+    assert(Is_mpf_class_Equals(c, expected.c_str()));
+    a /= b;
+    assert(Is_mpf_class_Equals(a, expected.c_str()));
+    std::cout << "Division Test passed." << std::endl;
+}
+void testSubtraction() {
+    mpf_class a(5.0);
+    mpf_class b(2.0);
+    std::string expected = "3.0000000000";
+
+    mpf_class c = a - b;
+    assert(Is_mpf_class_Equals(c, expected.c_str()));
+    a -= b;
+    assert(Is_mpf_class_Equals(a, expected.c_str()));
+    std::cout << "Subtraction Test passed." << std::endl;
+}
+
+void testEqNonEq() {
+    mpf_class obj1("2.718281828459045");
+    mpf_class obj2("2.718281828459045");
+    assert(obj1 == obj2);
+    std::cout << "Equality test passed." << std::endl;
+
+    mpf_class obj3("3.141592653589793");
+    assert(obj1 != obj3);
+    std::cout << "Inequality test passed." << std::endl;
+
+    mpf_class obj4("2.7182818284590451");
+    assert(obj1 != obj4);
+    std::cout << "Subtle difference test passed." << std::endl;
+}
+
 int main() {
-    ////////////////////////////////////////////////////////////////////////////////////////
-    // 5.1 Initialization Functions
-    ////////////////////////////////////////////////////////////////////////////////////////
     testDefaultPrecision();
     testDefaultConstructor();
     testCopyConstructor();
+    testAssignmentOperator();
+    testInitializationAndAssignmentDouble();
+    testInitializationAndAssignmentString();
+    testAddition();
+    testMultplication();
+    testDivision();
+    testSubtraction();
+    testEqNonEq();
     std::cout << "All tests passed." << std::endl;
 
     return 0;
