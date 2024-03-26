5f1ec39bb55794a3eaad6be90e390113e7436e61
NAKATA Maho <maho.nakata@gmail.com>
2024-03-26 11:54:54 +0900

    .

--- test_gmpxx_mkII.cpp
+++ test_gmpxx_mkII.cpp
@@ -493,9 +493,9 @@ void test_mpf_class_constructor_precision() {
     mpf_class y(3.5, 1024);
     mpf_class f6(x + y);
     assert(f6.get_prec() == 1024);
-    mpf_class z(3.5, 512);
+    mpf_class z(3.5, 2048);
     mpf_class f7(z + x);
-    assert(f7.get_prec() == 512);
+    assert(f7.get_prec() == 2048);
 #endif
     std::cout << "test_mpf_class_constructor_precision passed." << std::endl;
 }
