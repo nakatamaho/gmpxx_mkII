bdb98047c37845375655b2e94935db3a0e06ba9e
NAKATA Maho <maho.nakata@gmail.com>
2024-04-29 09:03:28 +0900

    .

--- benchmarks/00_inner_product/go.sh
+++ benchmarks/00_inner_product/go.sh
@@ -1,7 +1,11 @@
-./inner_product_gmp_10_naive         10000000 1024
-./inner_product_gmp_11_openmp        10000000 1024
-./inner_product_gmp_12_mpblas        10000000 1024
-./inner_product_gmp_12_mpblas_mkII   10000000 1024
-./inner_product_gmp_12_mpblas_compat 10000000 1024
-./inner_product_gmp_12_mpblas_mkIISR 10000000 1024
+./inner_product_gmp_10_naive                10000000 1024
+./inner_product_gmp_11_openmp               10000000 1024
+./inner_product_gmp_12_mpblas               10000000 1024
+./inner_product_gmp_12_mpblas_mkII          10000000 1024
+./inner_product_gmp_12_mpblas_compat        10000000 1024
+./inner_product_gmp_12_mpblas_mkIISR        10000000 1024
+./inner_product_gmp_13_mpblas_openmp        10000000 1024
+./inner_product_gmp_13_mpblas_openmp_compat 10000000 1024
+./inner_product_gmp_13_mpblas_openmp_mkII   10000000 1024
+./inner_product_gmp_13_mpblas_openmp_mkIISR 10000000 1024
 
