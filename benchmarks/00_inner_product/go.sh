354b9d6cb5590ae5a835e82bbe3bf3b916f579d5
NAKATA Maho <maho.nakata@gmail.com>
2024-05-24 14:53:05 +0900

    .

--- benchmarks/00_inner_product/go.sh
+++ benchmarks/00_inner_product/go.sh
@@ -1,13 +1,23 @@
 uname -a
 cat /proc/cpuinfo | grep 'model name' | head -1
-./inner_product_gmp_10_naive                10000000 1024
-./inner_product_gmp_11_openmp               10000000 1024
-./inner_product_gmp_12_mpblas_orig          10000000 1024
-./inner_product_gmp_12_mpblas_mkII          10000000 1024
-./inner_product_gmp_12_mpblas_compat        10000000 1024
-./inner_product_gmp_12_mpblas_mkIISR        10000000 1024
-./inner_product_gmp_13_mpblas_openmp        10000000 1024
-./inner_product_gmp_13_mpblas_openmp_compat 10000000 1024
-./inner_product_gmp_13_mpblas_openmp_mkII   10000000 1024
-./inner_product_gmp_13_mpblas_openmp_mkIISR 10000000 1024
-
+echo
+executables=(
+    "inner_product_gmp_10_naive"
+    "inner_product_gmp_11_openmp"
+    "inner_product_gmp_12_mpblas_orig"
+    "inner_product_gmp_12_mpblas_mkII"
+    "inner_product_gmp_12_mpblas_compat"
+    "inner_product_gmp_12_mpblas_mkIISR"
+    "inner_product_gmp_13_mpblas_openmp"
+    "inner_product_gmp_13_mpblas_openmp_compat"
+    "inner_product_gmp_13_mpblas_openmp_mkII"
+    "inner_product_gmp_13_mpblas_openmp_mkIISR"
+)
+for exe in "${executables[@]}"; do
+    echo "./$exe 10000000 1024"
+    ./$exe 10000000 1024
+    if [ -f gmon.out ]; then
+        mv gmon.out "gmon_${exe}.out"
+    fi
+    echo
+done
