9ed20aafd37600c71898fe7fb157353f9964e736
NAKATA Maho <maho.nakata@gmail.com>
2024-09-23 11:47:27 +0900

    fix typo

--- benchmarks/00_inner_product/go_perf.sh
+++ benchmarks/00_inner_product/go_perf.sh
@@ -6,7 +6,7 @@ FLAMEGRAPH_DIR="/home/docker/FlameGraph"
 
 executables=(
     "inner_product_gmp_C_native_10"
-    "inner_product_gmp_C_native_11_openmp"
+    "inner_product_gmp_C_native_openmp_11"
     "inner_product_gmp_kernel_01_orig"
     "inner_product_gmp_kernel_01_mkII"
     "inner_product_gmp_kernel_01_mkIISR"
