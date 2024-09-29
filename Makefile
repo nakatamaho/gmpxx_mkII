c9342ce9b25c96c0e88a7b8224303bdd23ca493a
NAKATA Maho <maho.nakata@gmail.com>
2024-09-29 16:33:06 +0900

    Add Rgemv

--- Makefile
+++ Makefile
@@ -58,13 +58,22 @@ Raxpy_gmp_kernel_02_orig Raxpy_gmp_kernel_02_mkII Raxpy_gmp_kernel_02_mkIISR \
 Raxpy_gmp_kernel_openmp_01_orig Raxpy_gmp_kernel_openmp_01_mkII Raxpy_gmp_kernel_openmp_01_mkIISR \
 Raxpy_gmp_kernel_openmp_02_orig Raxpy_gmp_kernel_openmp_02_mkII Raxpy_gmp_kernel_openmp_02_mkIISR)
 
+BENCHMARKS02_DIR = benchmarks/02_Rgemv
+BENCHMARKS02_0 = $(addprefix $(BENCHMARKS02_DIR)/,Rgemv_gmp_C_native_01)
+#BENCHMARKS02_0 = $(addprefix $(BENCHMARKS02_DIR)/,Rgemv_gmp_C_native_01 Rgemv_gmp_C_native_openmp_01)
+#BENCHMARKS02_1 = $(addprefix $(BENCHMARKS02_DIR)/,\
+#Rgemv_gmp_kernel_02_orig Rgemv_gmp_kernel_02_mkII Rgemv_gmp_kernel_02_mkIISR \
+#Rgemv_gmp_kernel_02_orig Rgemv_gmp_kernel_02_mkII Rgemv_gmp_kernel_02_mkIISR \
+#Rgemv_gmp_kernel_openmp_02_orig Rgemv_gmp_kernel_openmp_02_mkII Rgemv_gmp_kernel_openmp_02_mkIISR \
+#Rgemv_gmp_kernel_openmp_02_orig Rgemv_gmp_kernel_openmp_02_mkII Rgemv_gmp_kernel_openmp_02_mkIISR)
+
 BENCHMARKS03_DIR = benchmarks/03_Rgemm
 BENCHMARKS03_0 = $(addprefix $(BENCHMARKS03_DIR)/,Rgemm_gmp_10_naive_ijl Rgemm_gmp_11_naive_jli Rgemm_gmp_12_naive_jli_openmp)
 BENCHMARKS03_1 = $(addprefix $(BENCHMARKS03_DIR)/,Rgemm_gmp_20_mpblas_orig Rgemm_gmp_20_mpblas_mkII Rgemm_gmp_20_mpblas_mkIISR)
 BENCHMARKS03_2 = $(addprefix $(BENCHMARKS03_DIR)/,Rgemm_gmp_21_mpblas_openmp_orig Rgemm_gmp_21_mpblas_openmp_mkII Rgemm_gmp_21_mpblas_openmp_mkIISR)
 BENCHMARKS03_3 = $(addprefix $(BENCHMARKS03_DIR)/,Rgemm_gmp_30_mpblaslike_naive_ijl_orig Rgemm_gmp_30_mpblaslike_naive_ijl_mkII Rgemm_gmp_30_mpblaslike_naive_ijl_mkIISR)
 
-all: $(TARGET) $(TARGET_ORIG) $(TARGET_COMPAT) $(TARGET_MKIISR) $(TARGET_TEST_ENV) $(EXAMPLES_EXECUTABLES) $(ORIG_TESTS) $(BENCHMARKS00_0) $(BENCHMARKS00_1) $(BENCHMARKS01_0) $(BENCHMARKS01_1) $(BENCHMARKS03_0) $(BENCHMARKS03_1) $(BENCHMARKS03_2) $(BENCHMARKS03_3)
+all: $(TARGET) $(TARGET_ORIG) $(TARGET_COMPAT) $(TARGET_MKIISR) $(TARGET_TEST_ENV) $(EXAMPLES_EXECUTABLES) $(ORIG_TESTS) $(BENCHMARKS00_0) $(BENCHMARKS00_1) $(BENCHMARKS01_0) $(BENCHMARKS01_1) $(BENCHMARKS02_0) $(BENCHMARKS02_1) $(BENCHMARKS03_0) $(BENCHMARKS03_1) $(BENCHMARKS03_2) $(BENCHMARKS03_3)
 
 includedir = $(PREFIX)/include
 
@@ -235,6 +244,10 @@ $(BENCHMARKS01_DIR)/Raxpy_gmp_kernel_openmp_02_mkII: $(BENCHMARKS01_DIR)/Raxpy_g
 $(BENCHMARKS01_DIR)/Raxpy_gmp_kernel_openmp_02_mkIISR: $(BENCHMARKS01_DIR)/Raxpy_gmp_kernel_openmp_02.cpp $(HEADERS)
 	$(CXX) $(CXXFLAGS_BENCH) $(INCLUDES) $(GMPXX_MODE_MKIISR) -o $@ $< $(LDFLAGS) $(RPATH_FLAGS)
 	$(CXX) $(CXXFLAGS_BENCH) $(INCLUDES) $(GMPXX_MODE_MKIISR) -S -fverbose-asm -g -o $@.s $< $(LDFLAGS)
+
+$(BENCHMARKS02_DIR)/%: $(BENCHMARKS02_DIR)/%.cpp $(HEADERS)
+	$(CXX) $(CXXFLAGS_BENCH) $(INCLUDES) -o $@ $< $(LDFLAGS)
+	$(CXX) $(CXXFLAGS_BENCH) $(INCLUDES) -S -fverbose-asm -g -o $@.s $< $(LDFLAGS)
 
 $(BENCHMARKS03_DIR)/%: $(BENCHMARKS03_DIR)/%.cpp $(HEADERS)
 	$(CXX) $(CXXFLAGS_BENCH) $(INCLUDES) -o $@ $< $(LDFLAGS)
---
+++ benchmarks/02_Rgemv/Rgemv_gmp_C_native_01.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <chrono>
+#include <gmp.h>
+
+#if defined USE_ORIGINAL_GMPXX
+#include <gmpxx.h>
+#else
+#include "gmpxx_mkII.h"
+#if !defined ___GMPXX_STRICT_COMPATIBILITY___
+using namespace gmp;
+#endif
+#endif
+
+#include "Rgemv.hpp"
+
+#define MFLOPS 1e+6
+
+gmp_randstate_t state;
+
+// Reference implementation using mpf_t
+void _Rgemv(int64_t m, int64_t n, const mpf_t alpha, const mpf_t *A, int64_t lda, const mpf_t *x, int64_t incx, const mpf_t beta, mpf_t *y, int64_t incy) {
+
+    if (incx != 1 || incy != 1) {
+        std::cerr << "Increments other than 1 are not supported." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    mpf_t temp;
+    mpf_init(temp);
+
+    // Scale y by beta
+    for (int64_t i = 0; i < m; ++i) {
+        mpf_mul(temp, beta, y[i]); // temp = beta * y[i]
+        mpf_set(y[i], temp);       // y[i] = temp
+    }
+
+    // Compute alpha * A * x and add to y
+    for (int64_t j = 0; j < n; ++j) {
+        for (int64_t i = 0; i < m; ++i) {
+            mpf_mul(temp, alpha, A[i + j * lda]); // temp = alpha * A[i + j*lda]
+            mpf_mul(temp, temp, x[j]);            // temp = temp * x[j]
+            mpf_add(y[i], y[i], temp);            // y[i] += temp
+        }
+    }
+
+    mpf_clear(temp);
+}
+
+// Initialize a matrix with random values
+void init_mpf_mat(mpf_t *mat, int64_t m, int64_t n, int64_t lda, int prec) {
+    for (int64_t j = 0; j < n; ++j) {
+        for (int64_t i = 0; i < m; ++i) {
+            mpf_init2(mat[i + j * lda], prec);
+            mpf_urandomb(mat[i + j * lda], state, prec); // 0 <= mat[i + j*lda] < 1
+        }
+    }
+}
+
+// Clear a matrix
+void clear_mpf_mat(mpf_t *mat, int64_t m, int64_t n, int64_t lda) {
+    for (int64_t j = 0; j < n; ++j) {
+        for (int64_t i = 0; i < m; ++i) {
+            mpf_clear(mat[i + j * lda]);
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    gmp_randinit_default(state);
+    gmp_randseed_ui(state, 42);
+
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <rows m> <cols n> <precision>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    int64_t m = std::atoll(argv[1]);
+    int64_t n = std::atoll(argv[2]);
+    int prec = std::atoi(argv[3]);
+    mpf_set_default_prec(prec);
+
+    int64_t lda = m; // Leading dimension
+
+    // Allocate matrices and vectors
+    mpf_t *A = new mpf_t[m * n];
+    mpf_t *x = new mpf_t[n];
+    mpf_t *y = new mpf_t[m];
+
+    // Initialize alpha and beta
+    mpf_t alpha, beta;
+    mpf_init2(alpha, prec);
+    mpf_urandomb(alpha, state, prec);
+    mpf_init2(beta, prec);
+    mpf_urandomb(beta, state, prec);
+
+    // Initialize A, x, y with random values
+    init_mpf_mat(A, m, n, lda, prec);
+    for (int64_t i = 0; i < n; ++i) {
+        mpf_init2(x[i], prec);
+        mpf_urandomb(x[i], state, prec);
+    }
+    for (int64_t i = 0; i < m; ++i) {
+        mpf_init2(y[i], prec);
+        mpf_urandomb(y[i], state, prec);
+    }
+
+    // Initialize mpf_class versions for reference
+    mpf_class *A_mpf_class = new mpf_class[m * n];
+    mpf_class *x_mpf_class = new mpf_class[n];
+    mpf_class *y_mpf_class = new mpf_class[m];
+    mpf_class alpha_class = mpf_class(alpha);
+    mpf_class beta_class = mpf_class(beta);
+
+    for (int64_t j = 0; j < n; ++j) {
+        for (int64_t i = 0; i < m; ++i) {
+            A_mpf_class[i + j * lda] = mpf_class(A[i + j * lda]);
+        }
+    }
+    for (int64_t i = 0; i < n; ++i) {
+        x_mpf_class[i] = mpf_class(x[i]);
+    }
+    for (int64_t i = 0; i < m; ++i) {
+        y_mpf_class[i] = mpf_class(y[i]);
+    }
+
+    // Perform _Rgemv
+    auto start = std::chrono::high_resolution_clock::now();
+    _Rgemv(m, n, alpha, A, lda, x, 1, beta, y, 1);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    // Perform reference Rgemv
+    Rgemv("n", m, n, alpha_class, A_mpf_class, lda, x_mpf_class, 1, beta_class, y_mpf_class, 1);
+
+    // Calculate elapsed time for _Rgemv
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    double mflops = (2.0 * double(m) * double(n)) / (elapsed_seconds.count() * MFLOPS);
+
+    std::cout << "Elapsed time: " << elapsed_seconds.count() << " s" << std::endl;
+    std::cout << "MFLOPS: " << mflops << std::endl;
+
+    // Compute L1 norm of the difference
+    mpf_class l1_norm = 0;
+    for (int64_t i = 0; i < m; ++i) {
+        mpf_class diff = abs(mpf_class(y[i]) - y_mpf_class[i]);
+        l1_norm += diff;
+    }
+
+    std::cout << "L1 Norm of difference: " << l1_norm << std::endl;
+
+    mpf_class threshold = 1e-5;
+    if (l1_norm < threshold) {
+        std::cout << "Result OK" << std::endl;
+    } else {
+        std::cout << "Result NG" << std::endl;
+    }
+
+    // Clear memory
+    clear_mpf_mat(A, m, n, lda);
+    for (int64_t i = 0; i < n; ++i) {
+        mpf_clear(x[i]);
+    }
+    for (int64_t i = 0; i < m; ++i) {
+        mpf_clear(y[i]);
+    }
+    mpf_clear(alpha);
+    delete[] A;
+    delete[] x;
+    delete[] y;
+    delete[] A_mpf_class;
+    delete[] x_mpf_class;
+    delete[] y_mpf_class;
+
+    return EXIT_SUCCESS;
+}
