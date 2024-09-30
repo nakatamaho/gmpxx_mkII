469267bb7dbd10d6fd0669e6b716d70004a1a19e
NAKATA Maho <maho.nakata@gmail.com>
2024-09-30 17:42:51 +0900

    .

--- benchmarks/03_Rgemm/Rgemm_gmp_kernel_03.cpp
+++ benchmarks/03_Rgemm/Rgemm_gmp_kernel_03.cpp
@@ -38,10 +38,10 @@ void _Rgemm(int64_t m, int64_t k, int64_t n, const mpf_class &alpha, const mpf_c
     }
 
     // Compute alpha * A * B and add to C: C += alpha * A * B
-    mpf_class templ;
+    mpf_class temp, templ;
     for (int64_t j = 0; j < m; ++j) {
         for (int64_t l = 0; l < k; ++l) {
-            mpf_class temp = alpha;
+            temp = alpha;
             temp *= B[l + j * ldb];
             for (int64_t i = 0; i < m; ++i) {
                 templ = temp;
