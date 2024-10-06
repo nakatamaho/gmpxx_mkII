42ac283e396ca287d08adcca53d268e718a1c472
NAKATA Maho <maho.nakata@gmail.com>
2024-10-06 19:25:25 +0900

    .

--- benchmarks/01_Raxpy/plot.py
+++ benchmarks/01_Raxpy/plot.py
@@ -138,6 +138,8 @@ for file_path in file_paths:
 
     filename = f'singlecore_operations_{cpu_model_filename}_{dim}_{prec}.pdf'
     plt.savefig(filename)
+    filename = f'singlecore_operations_{cpu_model_filename}_{dim}_{prec}.png'
+    plt.savefig(filename)
     plt.close()
 
     # Plotting only "openmp" operations if any exist
@@ -165,5 +167,7 @@ for file_path in file_paths:
             text.set_fontweight('bold')
 
         filename_openmp = f'openmp_operations_{cpu_model_filename}_{dim}_{prec}.pdf'
+        plt.savefig(filename_openmp, bbox_inches='tight')
+        filename_openmp = f'openmp_operations_{cpu_model_filename}_{dim}_{prec}.png'
         plt.savefig(filename_openmp, bbox_inches='tight')
         plt.close()
