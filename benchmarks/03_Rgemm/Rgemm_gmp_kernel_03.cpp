62036bac890478c3209b98c5d410add2e7dfb0ac
NAKATA Maho <maho.nakata@gmail.com>
2025-01-09 16:15:07 +0900

    fid the loop index

--- benchmarks/03_Rgemm/Rgemm_gmp_kernel_03.cpp
+++ benchmarks/03_Rgemm/Rgemm_gmp_kernel_03.cpp
@@ -39,7 +39,7 @@ void _Rgemm(int64_t m, int64_t k, int64_t n, const mpf_class &alpha, const mpf_c
 
     // Compute alpha * A * B and add to C: C += alpha * A * B
     mpf_class temp, templ;
-    for (int64_t j = 0; j < m; ++j) {
+    for (int64_t j = 0; j < n; ++j) {
         for (int64_t l = 0; l < k; ++l) {
             temp = alpha;
             temp *= B[l + j * ldb];
