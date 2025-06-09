f0ed5b24e8ea8e46085d2a4cd43f49be794455de
NAKATA Maho <maho.nakata@gmail.com>
2025-06-09 16:11:27 +0900

    .

--- indent.sh
+++ indent.sh
@@ -1,5 +1,10 @@
-FILES=`ls *.cpp *.cc *.h  *.h.in`
-
-for _file in $FILES; do
-clang-format -style="{BasedOnStyle: llvm, IndentWidth: 4, ColumnLimit: 10000, SortIncludes: false}" $_file > ${_file}__ ; mv ${_file}__ ${_file}
+for f in *.cpp *.cc *.h *.h.in; do
+  clang-format-19 -i -style '{
+    BasedOnStyle: llvm,
+    IndentWidth: 4,
+    ColumnLimit: 0,
+    SortIncludes: false,
+    AlignEscapedNewlines: LeftWithLastLine
+  }' "$f"
 done
+
