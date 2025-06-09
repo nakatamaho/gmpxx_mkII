e73f75f782de4a446c6de1f438c89ac3103124e3
NAKATA Maho <maho.nakata@gmail.com>
2025-06-09 16:32:52 +0900

    .

--- indent.sh
+++ indent.sh
@@ -2,7 +2,7 @@ for f in *.cpp *.cc *.h *.h.in; do
   clang-format-19 -i -style '{
     BasedOnStyle: llvm,
     IndentWidth: 4,
-    ColumnLimit: 0,
+    ColumnLimit: 10000,
     SortIncludes: false,
     AlignEscapedNewlines: LeftWithLastLine
   }' "$f"
